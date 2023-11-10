d7962256f5304f87a461cff53a933cfb1ede067b
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-11-10 19:03:13 +0100

    Update fetchFotoApi.js
    Importing the Axios library that allows you to conveniently perform HTTP queries
    Pixabay API key
    Pixabay API URL
    Number of images per page
    Function to download photos from Pixabay based on query and page number
    Invoking an HTTP request using Axios
    Logging responses for diagnostic purposes
    Returning data from the response

---
+++ src/components/services/fetchFotoApi.js
@@ -0,0 +1,25 @@
+import axios1 from 'axios';
+
+const KEY = '40494461-a7c535355632b3301706f2786';
+const API_URL = 'https://pixabay.com/api/';
+let perPage = 40;
+
+export const fetchFotos = async (query, page) => {
+  const response = await axios1.get(API_URL, {
+    params: {
+      key: KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesear: true,
+      per_page: perPage,
+      page: page,
+    },
+  });
+
+  console.log('fetchfotos');
+  console.log(response);
+
+  // Zwrócenie danych z odpowiedzi
+  return response.data;
+};

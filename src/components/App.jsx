77c1a01b94965ed31bdb654b41b9c6f27f786f7c
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-11-11 10:53:39 +0100

    working
    adding axios & repair app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import '.App.css';
-import { fetchFotos } from '../components/services/fetchFotos';
-import { SearchgBar } from './SearchBar';
+// import './App.css';
+import { fetchFotos } from './services/fetchFotoApi';
+import { SearchBar } from './SearchBar';
 import { ImageGallery } from './ImageGallery/';
 import { ImageGalleryItem } from './ImageGalleryItem';
 import { Button } from './Button';
 import { Loader } from './Loader';
 import { Modal } from './Modal';
 
-const getQuery = event => {
-  event.preventDefault();
-  const searchKeyWord = event.target.elements.searchQuery.value;
-  if (searchKeyWord !== searchQuery) {
-    setSearchQuery(searchKeyWord);
-    setCurrentPage(1);
-    setImages([]);
-  }
-};
-
-const getURL = (imageURL, alt) => {
-  setImageURL(imageURL);
-  setImageAlt(alt);
-  setIsModalOpen(true);
-};
-
-useEffect(() => {
-  const showImages = async () => {
-    setIsLoading(true);
-    try {
-      const data = await fetchFotos(searchQuery, currentPage);
-      const images = data.hits;
-      setTotalPages(Math.ceil(data.total / 40));
-      if (currentPage === 1) {
-        setImages(images);
-      } else {
-        setImages(prevImages => [...prevImages, ...images]);
-      }
-    } catch (error) {
-      setError(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-  showImages();
-}, [searchQuery, currentPage]);
-
-const loadMore = event => {
-  event.preventDefault();
-  setCurrentPage(prevCurrentPage => prevCurrentPage + 1);
-};
-
-const App = () => {
+export const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const [images, setImages] = useState([]);
@@ -61,8 +19,50 @@ const App = () => {
   const [imageURL, setImageURL] = useState('');
   const [imageAlt, setImageAlt] = useState('');
 
-  const closrModal = () => {
+  const closeModal = () => {
     setIsModalOpen(false);
+  };
+
+  const getQuery = event => {
+    event.preventDefault();
+    const searchKeyWord = event.target.elements.searchQuery.value;
+    if (searchKeyWord !== searchQuery) {
+      setSearchQuery(searchKeyWord);
+      setCurrentPage(1);
+      setImages([]);
+    }
+  };
+
+  const getURL = (imageURL, alt) => {
+    setImageURL(imageURL);
+    setImageAlt(alt);
+    setIsModalOpen(true);
+  };
+
+  useEffect(() => {
+    const showImages = async () => {
+      setIsLoading(true);
+      try {
+        const data = await fetchFotos(searchQuery, currentPage);
+        const images = data.hits;
+        setTotalPages(Math.ceil(data.total / 40));
+        if (currentPage === 1) {
+          setImages(images);
+        } else {
+          setImages(prevImages => [...prevImages, ...images]);
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    showImages();
+  }, [searchQuery, currentPage]);
+
+  const loadMore = event => {
+    event.preventDefault();
+    setCurrentPage(prevCurrentPage => prevCurrentPage + 1);
   };
 
   return (
@@ -70,19 +70,14 @@ const App = () => {
       {isModalOpen && (
         <Modal modalURL={imageURL} alt={imageAlt} onClose={closeModal} />
       )}
-
       <SearchBar getQuery={getQuery} />
-
       {error && <h1>Oops, something went wrong</h1>}
-
       {isLoading && <Loader />}
-
       {images.length !== 0 && (
         <ImageGallery>
           <ImageGalleryItem data={images} saveURL={getURL} />
         </ImageGallery>
       )}
-
       {images.length !== 0 && currentPage !== totalPages && (
         <Button onClick={loadMore} />
       )}
